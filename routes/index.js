d81da41a20ba7b486cfdff8dd0a7219b315ebd43
atran <atran@rollbar.com>
2021-01-15 00:26:41 -0800

    Add node version to package

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
-  res.fruitbag.banana;
+  Object.keys(res.fruitbag.banana);
 });
 
 module.exports = router;
