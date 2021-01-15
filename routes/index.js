2eb50f01d364f155dfbf608a6d5aa43782d7f383
atran <atran@rollbar.com>
2021-01-15 00:38:32 -0800

    Add rollbar browserjs and error

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-  Object.keys(res.fruitbag.banana);
+  res.sendFile(path.join(__dirname + '/html/index.html'));
 });
 
 module.exports = router;
